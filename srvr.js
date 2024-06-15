9b6e47f4170e86c6579d8b6bf92d70b9f9371294
SAGGAM BHARGAV <bhargavsaggam@gmail.com>
2024-06-15 21:24:28 +0530

    ftg

--- srvr.js
+++ srvr.js
@@ -92,4 +92,7 @@ app.post('/verify', async (req, res) => {
     }
 })
 
- 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
